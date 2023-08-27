d904245b920de4080e3bcbcc64e3e37066a0be97
Em3L <martink_lopez@yahoo.com>
2023-08-27 20:27:03 +1000

    changed server code: const express = require("express"); const exphbs = require("express-handlebars"); const path = require("path"); const routes = require("./controllers");
    const app = express();
    
    app.use(routes);
    
    // Set up Handlebars as the view engine
    app.engine("handlebars", exphbs());
    app.set("view engine", "handlebars");
    app.set("views", path.join(__dirname, "views"));
    
    app.get("/", (req, res) => {
      res.render("home"); // Render the view using the default layout file (main.handlebars)
    });
    
    const PORT = process.env.PORT || 3000;
    app.listen(PORT, () => {
      console.log("Server listening on: http://localhost:" + PORT);
    });

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
-// Import required packages
-const express = require('express');
-const exphbs = require('express-handlebars');
-const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const db = require('./models');
-const dotenv = require('dotenv');
-const bcrypt = require('bcrypt');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const routes = require("./controllers");
+
+const app = express();
+
+app.use(routes);
+
+// Set up Handlebars as the view engine
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+  res.render("home"); // Render the view using the default layout file (main.handlebars)
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
