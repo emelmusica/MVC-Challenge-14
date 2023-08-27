0cc2f785762106ce509b41dd9739ed391090627c
Em3L <martink_lopez@yahoo.com>
2023-08-27 20:13:16 +1000

    deleted helper comment

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const db = require('./models'); // Assuming your models are in a 'models' folder
+const db = require('./models');
 const dotenv = require('dotenv');
 const bcrypt = require('bcrypt');
