ecf04e347984dd4cf6aef8f323cc9633e1acec3a
Em3L <martink_lopez@yahoo.com>
2023-09-05 21:04:00 +1000

    Finished code for server

--- server.js
+++ server.js
@@ -1,22 +1,53 @@
 const express = require("express");
+const session = require("express-session");
 const exphbs = require("express-handlebars");
 const path = require("path");
-const routes = require("./controllers");
-
 const app = express();
+const PORT = process.env.PORT || 3000;
+const routes = require("./controllers");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
+const sequelize = require("./config/connection");
+
+// Middleware
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Sets up session for cookies and connect to our Sequelize db
+const sess = {
+  secret: "Super secret secret",
+  cookie: {
+    maxAge: 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax",
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
 
 app.use(routes);
 
-// Set up Handlebars as the view engine
-app.engine("handlebars", exphbs());
+// View engine setup
+app.engine(
+  "handlebars",
+  exphbs({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
 app.set("view engine", "handlebars");
 app.set("views", path.join(__dirname, "views"));
 
-app.get("/", (req, res) => {
-  res.render("home"); // Render the view using the default layout file (main.handlebars)
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+// Start the server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
