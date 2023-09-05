f272cbddf991594d4d292414ec6d9660e384bdff
Em3L <martink_lopez@yahoo.com>
2023-09-05 21:10:01 +1000

    updated connection js file

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,21 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const db = require('../models'); 
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const router = express.Router();
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
