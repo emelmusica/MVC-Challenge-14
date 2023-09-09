19465edc7ad6cdfe8d88b7a8aadbc69762425eb0
Em3L <martink_lopez@yahoo.com>
2023-09-09 20:36:52 +1000

    fixed all code

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -3,28 +3,29 @@ const router = require("express").Router();
 
 // Get all categories
 router.get("/", async (req, res) => {
-    try {
-        const categoryData = await Category.findAll();
-    } Catch (error) {
-        console.error("Error Fetching all Categories", error);
-        res.status(500).json(error);
-    }
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (error) {
+    console.error("Error fetching all categories:", error);
+    res.status(500).json(error);
+  }
 });
 
 // Get a specific category by ID
 router.get("/:id", async (req, res) => {
     try {
-        const categoryData = await Category.findByPk(req.params.id);
-        if (!categoryData) {
-            return res
-            .status(404)
-            .json{{ message: "no category found with this ID"});
-        }
-        res.status(200).json(categoryData);
+      const categoryData = await Category.findByPk(req.params.id);
+      if (!categoryData) {
+        return res
+          .status(404)
+          .json({ message: "No category found with this ID!" });
+      }
+      res.status(200).json(categoryData);
     } catch (error) {
-        console.error('Error fetching category with ID ${req.params.id}:', error);
-        res.status(500).json(error);
+      console.error(`Error fetching category with ID ${req.params.id}:`, error);
+      res.status(500).json(error);
     }
-});
-
-module.exports = router;
+  });
+  
+  module.exports = router;
