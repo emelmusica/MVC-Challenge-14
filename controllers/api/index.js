38b5476096024e5559158da65bca056a8f592d0b
Em3L <martink_lopez@yahoo.com>
2023-09-09 20:40:37 +1000

    created a rough draft for index js file

---
+++ controllers/api/index.js
@@ -0,0 +1,11 @@
+const router = require("express").Router();
+//const // = require("./p");
+//const categoryRoutes = require("./categoryRoutes");
+const userRoutes = require("./userRoutes");
+
+
+// router.use("/", );
+// router.use("/categoryRoutes", categoryRoutes);
+// router.use("/userRoutes", userRoutes);
+
+module.exports = router;
