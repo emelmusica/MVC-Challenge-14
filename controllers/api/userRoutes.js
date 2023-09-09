5f1c658dc3b014f039608d12f3cd605d85ccc9ca
Em3L <martink_lopez@yahoo.com>
2023-09-09 21:16:03 +1000

    userData code was broken

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -3,14 +3,14 @@ const { User } = require("../../models");
 
 // get userData test
 router.get("/", async (req, res) => {
-    try {
-      const userData = await User.findAll();
-      res.status(200).json(userData);
-    } catch (error) {
-        console.error("Error fetching all users:" error);
-        res.status(500).json({ message: "Internal Server Error", error });
-    }
-});    
+  try {
+    const userData = await User.findAll();
+    res.status(200).json(userData);
+  } catch (error) {
+    console.error("Error fetching all users:", error);
+    res.status(500).json({ message: "Internal Server Error", error });
+  }
+});
 
 // Login post request
 router.post("/login", async (req, res) => {
