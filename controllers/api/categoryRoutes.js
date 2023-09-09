e8b75e2021472b63788f76c744338dc216b25a94
Em3L <martink_lopez@yahoo.com>
2023-09-09 20:20:43 +1000

    get all categories code

--- controllers/api/categoryRoutes.js
+++ controllers/api/categoryRoutes.js
@@ -2,5 +2,13 @@ const { Category } = require("../../models");
 const router = require("express").Router();
 
 // Get all categories
+router.get("/", async (req, res) => {
+    try {
+        const categoryData = await Category.findAll();
+    } Catch (error) {
+        console.error("Error Fetching all Categories", error);
+        res.status(500).json(error);
+    }
+});
 
 // Get a specific category by ID
