9b3093ba411f27d8dd6a05cf4beb0d353948b2d3
Em3L <martink_lopez@yahoo.com>
2023-09-09 21:04:54 +1000

    added login and logout post request

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,6 +12,70 @@ router.get("/", async (req, res) => {
     }
 });    
 
-// login post request
-
-// post request logout
+// Login post request
+router.post("/login", async (req, res) => {
+    try {
+      const userData = await User.findOne({ where: { email: req.body.email } });
+  
+      if (!userData) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect email or password, please try again" });
+      }
+  
+      const validPassword = await userData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        return res
+          .status(401)
+          .json({ message: "Incorrect email or password, please try again" });
+      }
+  
+      req.session.save((error) => {
+        if (error) {
+          console.error("Session save error:", error);
+          return res.status(500).json({ message: "Error saving session." });
+        }
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.json({ message: "Login successful", user_id: userData.id });
+      });
+    } catch (error) {
+      console.error("Error during login:", error);
+      res.status(400).json({ message: "An error occurred" });
+    }
+  });
+  
+  router.post("/signUp", async (req, res) => {
+    try {
+      const userData = await User.create(req.body);
+  
+      req.session.save(() => {
+        req.session.user_id = userData.id;
+        req.session.logged_in = true;
+  
+        res.status(200).json(userData);
+      });
+    } catch (error) {
+      console.error("Error during sign-up:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  });
+  
+  // Post request to logout
+  router.post("/logout", (req, res) => {
+    if (req.session.logged_in) {
+      req.session.destroy((error) => {
+        if (error) {
+          console.error("Error during session destroy:", error);
+          return res.status(500).json({ message: "Error logging out." });
+        }
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+  
+  module.exports = router;
