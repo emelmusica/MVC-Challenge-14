81a50cbdd0b0e892c70d42cf9ad482dfa22ea902
Em3L <martink_lopez@yahoo.com>
2023-09-09 20:57:24 +1000

    finished get userData Test code

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -6,6 +6,11 @@ router.get("/", async (req, res) => {
     try {
       const userData = await User.findAll();
       res.status(200).json(userData);
+    } catch (error) {
+        console.error("Error fetching all users:" error);
+        res.status(500).json({ message: "Internal Server Error", error });
+    }
+});    
 
 // login post request
 
