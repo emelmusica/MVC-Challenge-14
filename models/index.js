b2f3cc38f91223ef53829f5b6ae0ee191d497800
Em3L <martink_lopez@yahoo.com>
2023-09-09 21:33:27 +1000

    I have to finish this index.js

---
+++ models/index.js
@@ -0,0 +1,14 @@
+// const  = require("./");
+const Category = require("./Category");
+const User = require("./User");
+
+// "".belongsTo(Category, {
+//   foreignKey: "category_id",
+// });
+
+// Category.hasMany("", {
+//   foreignKey: "category_id",
+//   onDelete: "CASCADE",
+// });
+
+module.exports = { , Category, User };
