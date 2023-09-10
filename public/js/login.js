6f28cd6ae7e4873caf496a86cb45b63b3a1c4149
Em3L <martink_lopez@yahoo.com>
2023-09-10 16:17:44 +1000

    created login js

---
+++ public/js/login.js
@@ -0,0 +1,72 @@
+console.log("Login script loaded!");
+
+const loginFormHandler = async (event) => {
+  event.preventDefault();
+  console.log("Login form submitted");
+
+  // Collect values from the login form
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  console.log("Sending email and password:", { email, password });
+
+  if (email && password) {
+    try {
+      const response = await fetch("/api/userRoutes/login", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: { "Content-Type": "application/json" },
+      });
+      console.log("API Response:", response);
+
+      if (response.ok) {
+        console.log(response.statusText);
+        document.location.replace("/");
+      } else {
+        alert(response.statusText);
+      }
+    } catch (error) {
+      console.error("There was an error:", error);
+      alert(
+        "There was an error processing your request. Please try again later."
+      );
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector("#signup-name").value.trim();
+  const email = document.querySelector("#signup-email").value.trim();
+  const password = document.querySelector("#signup-password").value.trim();
+
+  try {
+    const response = await fetch("/api/userRoutes/signUp", {
+      method: "POST",
+      body: JSON.stringify({ name, email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      // If successful, redirect the browser to the home page
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+    }
+  } catch (error) {
+    console.error("There was an error during the signup process:", error);
+    alert(
+      "There was an error processing your sign up request. Please try again later."
+    );
+  }
+};
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+console.log("Login form listener attached!");
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
+console.log("Signup form listener attached!");
