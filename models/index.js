fba406dc15174f14cdc7790b4aae21f875ccb191
Em3L <martink_lopez@yahoo.com>
2023-09-11 21:27:03 +1000

    blaaah

--- models/index.js
+++ models/index.js
@@ -2,14 +2,13 @@ const Sequelize = require("sequelize");
 const Category = require("./Category");
 const User = require("./User");
 
-
+// Initialize Sequelize with DATABASE_URL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
 });
-
 
 Category.init(sequelize, Sequelize);
 User.init(sequelize, Sequelize);
-
 
 Category.hasMany(User);
 User.belongsTo(Category);
