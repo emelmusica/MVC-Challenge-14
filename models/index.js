7c7679ca5638a534cc2524fd0d95cc4cc3ebe233
Em3L <martink_lopez@yahoo.com>
2023-09-11 22:18:23 +1000

    fixed index

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const Category = require("./Category");
-const User = require("./User");
+const User = require("./user");
+const Post = require("./post");
+const Comment = require("./comment");
 
 // Initialize Sequelize with DATABASE_URL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
@@ -9,8 +11,20 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 
 Category.init(sequelize, Sequelize);
 User.init(sequelize, Sequelize);
+Post.init(sequelize, Sequelize);
+Comment.init(sequelize, Sequelize);
 
 Category.hasMany(User);
 User.belongsTo(Category);
 
-module.exports = { sequelize, Category, User };
+
+Post.belongsTo(User); // Each post belongs to a user
+User.hasMany(Post);   // Each user can have multiple posts
+
+Post.hasMany(Comment); // Each post can have multiple comments
+Comment.belongsTo(Post); // Each comment belongs to a post
+
+Comment.belongsTo(User); // Each comment belongs to a user
+User.hasMany(Comment);   // Each user can have multiple comments
+
+module.exports = { sequelize, Category, User, Post, Comment };
